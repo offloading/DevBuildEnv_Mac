0cfb35d7ed300c9f12388e9760bbc36428a7e923
trainin9 <trainin9@trainin9-no-MacBook-Air.local>
2019-01-15 23:46:06 +0900

    first commit

---
+++ install_brew.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#xcode-select --install
+#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#brew update
+#brew config
+#brew install argon/mas/mas
+brew install rcmdnk/file/brew-file
